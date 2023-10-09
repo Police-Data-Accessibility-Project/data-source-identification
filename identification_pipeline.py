69babe3c97cb0a5d66bda506173d9e787756dfde
Marty <mbodenator@gmail.com>
2023-10-09 13:21:04 -0400

    wip

--- identification_pipeline.py
+++ identification_pipeline.py
@@ -2,6 +2,7 @@ import requests
 import os
 import polars as pl
 from html_tag_collector.collector import collector_main
+from datetime import datetime as dt
 
 identify_df = pl.read_csv("urls_to_identify.csv")
 identify_df.unique()
@@ -12,10 +13,11 @@ api_key = "Bearer " + os.getenv("PDAP_API_KEY")
 response = requests.get("https://data-sources.pdap.io/archives", headers={"Authorization": api_key})
 data = response.json()
 data_sources_df = pl.DataFrame(data)
-
+print(data_sources_df.head(100))
 duplicate_check_df = identify_df.join(data_sources_df, left_on="url", right_on="source_url", how="outer")
 non_duplicates_df = duplicate_check_df.filter(pl.col("id").is_null()).select(pl.col("url","id"))
 
-tagged_df = collector_main(non_duplicates_df)
-
+print(dt.now())
+tagged_df = collector_main(non_duplicates_df.head(100))
+print(dt.now())
 print(tagged_df)
