7caa00c49f61f638905e5cd742c6d73e691ae2d5
Kylie <github.a5cee@kyscott.aleeas.com>
2023-10-12 17:29:25 -0400

    Format with black

--- html_tag_collector/collector.py
+++ html_tag_collector/collector.py
@@ -12,7 +12,7 @@ from bs4 import BeautifulSoup
 
 
 # Define the list of header tags we want to extract
-header_tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+header_tags = ["h1", "h2", "h3", "h4", "h5", "h6"]
 
 
 def process_urls(urls):
@@ -23,19 +23,19 @@ def process_urls(urls):
 
     Returns:
         list: List of dicts with HTML tags.
-    """    
+    """
     results = []
     new_urls = ["https://" + url if not url.startswith("http") else url for url in urls]
 
     with ThreadPoolExecutor(max_workers=100) as executor:
         print("Retrieving HTML tags...")
         future_to_url = [executor.submit(get_response, url, i) for i, url in enumerate(new_urls)]
-    
+
         for future in tqdm(as_completed(future_to_url), total=len(future_to_url)):
             data = future.result()
             results.append(data)
 
-    results.sort(key=lambda d : d["index"])
+    results.sort(key=lambda d: d["index"])
     urls_and_responses = [{"url": urls[i], "response": result["response"]} for i, result in enumerate(results)]
 
     urls_and_headers = []
@@ -46,11 +46,11 @@ def process_urls(urls):
     for row in tqdm(urls_and_responses):
         if tags is not None:
             urls_and_headers.append(tags)
-        
+
         tags = {}
         res = row["response"]
         tags["url"] = row["url"]
-        
+
         if res is None:
             tags["http_response"] = "Request failed"
             continue
@@ -66,10 +66,10 @@ def process_urls(urls):
 
         tags["html_title"] = soup.title.string if soup.title is not None else ""
 
-        meta_tag = soup.find('meta', attrs={"name": "description"})
+        meta_tag = soup.find("meta", attrs={"name": "description"})
         try:
             tags["meta_description"] = meta_tag["content"] if meta_tag is not None else ""
-        except (KeyError):
+        except KeyError:
             tags["meta_description"] = ""
 
         html_headers = []
@@ -80,9 +80,6 @@ def process_urls(urls):
         tags["html_headers"] = html_headers
 
     urls_and_headers.append(tags)
-
-    with open('urls_and_headers.json', 'w') as f:
-        json.dump(urls_and_headers, f, indent=4)
 
     return urls_and_headers
 
@@ -95,42 +92,47 @@ def get_response(url, index):
 
     Returns:
         dict(int, Response): Dictionary of the url's index value and Response object, None if an error occurred.
-    """    
-    
+    """
+
     headers = {
         # Some websites refuse the connection of automated requests, setting the User-Agent will circumvent that
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
         # Make sure there's no pre-mature closing of responses before a redirect completes
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"}
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    }
     response = None
-    debug = True # Set to True to enable debug output
+    debug = True  # Set to True to enable debug output
     url = url.removesuffix(".json")
 
     try:
         response = requests.get(url, headers=headers, timeout=60)
     except requests.exceptions.SSLError:
-    # This error is raised when the website uses a legacy SSL version, which is not supported by requests
-        if debug: print("SSLError:", url)
+        # This error is raised when the website uses a legacy SSL version, which is not supported by requests
+        if debug:
+            print("SSLError:", url)
         # Retry using legacy SSL session
         response = get_legacy_session().get(url, headers=headers, timeout=60)
     except requests.exceptions.ConnectionError:
-    # Sometimes this error is raised because the provided url uses http when it should be https and the website does not handle it properly
-        if debug: print("MaxRetryError:", url)
+        # Sometimes this error is raised because the provided url uses http when it should be https and the website does not handle it properly
+        if debug:
+            print("MaxRetryError:", url)
 
         if not url[4] == "s":
             url = url[:4] + "s" + url[4:]
             # Retry with https
             response = requests.get(url, headers=headers, timeout=60)
     except (urllib3.exceptions.LocationParseError, requests.exceptions.ReadTimeout) as e:
-        if debug: print(f"{type(e).__name__}: {url}")
+        if debug:
+            print(f"{type(e).__name__}: {url}")
     except Exception as e:
         if debug:
             print("Exception:", url)
             print(traceback.format_exc())
             print(str(e))
     finally:
-        if debug: print(url, response)
-        
+        if debug:
+            print(url, response)
+
         return {"index": index, "response": response}
 
 
@@ -144,15 +146,15 @@ class CustomHttpAdapter(requests.adapters.HTTPAdapter):
 
     def init_poolmanager(self, connections, maxsize, block=False):
         self.poolmanager = urllib3.poolmanager.PoolManager(
-            num_pools=connections, maxsize=maxsize,
-            block=block, ssl_context=self.ssl_context)
+            num_pools=connections, maxsize=maxsize, block=block, ssl_context=self.ssl_context
+        )
 
 
 def get_legacy_session():
     ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
     ctx.options |= 0x4  # OP_LEGACY_SERVER_CONNECT
     session = requests.session()
-    session.mount('https://', CustomHttpAdapter(ctx))
+    session.mount("https://", CustomHttpAdapter(ctx))
     return session
 
 
@@ -166,26 +168,26 @@ def process_url(url, d={}):
         return False
 
     # Add the HTTP response status code as a new property
-    d['http_response'] = response.status_code
+    d["http_response"] = response.status_code
 
     # If the response status code is not in the 200 range, skip adding other properties
     if not response.ok:
         return
-    
+
     # Check if the URL ends with .pdf
-    if url.endswith('.pdf'):
+    if url.endswith(".pdf"):
         return
 
     # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, "html.parser")
 
     # Extract the title tag and its content
-    
+
     html_title = soup.title.string
 
     # Extract the meta description tag and its content
-    meta_tag = soup.find('meta', attrs={'name': 'description'})
-    meta_description = meta_tag['content'] if meta_tag is not None else ""
+    meta_tag = soup.find("meta", attrs={"name": "description"})
+    meta_description = meta_tag["content"] if meta_tag is not None else ""
 
     # Extract the header tags and their content
     html_headers = []
@@ -195,29 +197,29 @@ def process_url(url, d={}):
         html_headers.append({header_tag: header_content})
 
     # Add the new properties to the JSON object
-    d['html_title'] = html_title
-    d['meta_description'] = meta_description
-    d['html_headers'] = html_headers
+    d["html_title"] = html_title
+    d["meta_description"] = meta_description
+    d["html_headers"] = html_headers
 
     return d
 
 
 def main():
     # Open the input file and load the JSON data
-    with open('urls.json') as f:
+    with open("urls.json") as f:
         data = json.load(f)
 
     # Loop over each URL in the data
     valid_data = []
     for d in data:
-        url = d['url']
+        url = d["url"]
         if process_url(url, d) != False:
             valid_data.append(d)
 
     # Write the updated JSON data to a new file
-    with open('urls_and_headers.json', 'w') as f:
+    with open("urls_and_headers.json", "w") as f:
         json.dump(valid_data, f, indent=4)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
