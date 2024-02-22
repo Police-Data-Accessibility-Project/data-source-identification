3016443069a973dfff8cfadf78c77528ca4c2e8e
Kylie <github.a5cee@kyscott.aleeas.com>
2024-02-22 14:20:07 -0700

    Tag collector fixes

--- html_tag_collector/collector.py
+++ html_tag_collector/collector.py
@@ -3,7 +3,9 @@ import ssl
 import urllib3
 import urllib.request
 from concurrent.futures import as_completed, ThreadPoolExecutor
+import sys
 import traceback
+import multiprocessing
 
 import requests
 from requests_html import AsyncHTMLSession
@@ -13,29 +15,28 @@ from tqdm import tqdm
 from tqdm.asyncio import tqdm
 import bs4
 from bs4 import BeautifulSoup
-import sys
 import polars as pl
+
 
 # Define the list of header tags we want to extract
 header_tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
 DEBUG = False # Set to True to enable debug output
-VERBOSE = True # Set to True to print dataframe each batch
+VERBOSE = False # Set to True to print dataframe each batch
 
-# Define a function to process a URL and update the JSON object
-def process_urls(df, render_javascript=False):
+def process_urls(manager_list, render_javascript):
     """Process a list of urls and retrieve their HTML tags.
 
     Args:
-        df (polars dataframe): contains url column with relevant urls
-        render_javascript (bool): Whether or not to render webpage's JavaScript rendered HTML. Default is False.
+        manager_list (shared list[polars dataframe]): shared list containing the polars dataframe, used to send and retrieve data from outside the current process.
+        render_javascript (bool): Whether or not to render webpage's JavaScript rendered HTML.
 
     Returns:
         list: List of dicts with HTML tags.
     """
-
+    df = manager_list[0]
     urls = df.select(pl.col("url")).rows()
     new_urls = ["https://" + url[0] if not url[0].startswith("http") else url[0] for url in urls]
-    
+
     loop = asyncio.get_event_loop()
     loop.set_exception_handler(exception_handler)
     future = asyncio.ensure_future(run_get_response(new_urls))
@@ -44,7 +45,7 @@ def process_urls(df, render_javascript=False):
 
     results.sort(key=lambda d: d["index"])
     urls_and_responses = [{"index": result["index"], "url": urls[i], "response": result["response"]} for i, result in enumerate(results)]
-    
+
     if render_javascript:
         future = asyncio.ensure_future(render_js(urls_and_responses))
         loop.run_until_complete(future)
@@ -62,9 +63,10 @@ def process_urls(df, render_javascript=False):
     urls_and_headers = sorted(parsed_data, key=lambda d: d["index"])
     [url_headers.pop("index") for url_headers in urls_and_headers]
     header_tags_df = pl.DataFrame(urls_and_headers)
-    clean_header_tags_df = header_tags_df.with_columns(pl.col(["html_title", "meta_description"]).fill_null(""))
+    clean_header_tags_df = header_tags_df.with_columns(pl.all().fill_null(""))
 
-    return clean_header_tags_df
+    # Return updated DataFrame
+    manager_list[0] = clean_header_tags_df
 
 
 def exception_handler(loop, context):
@@ -119,7 +121,7 @@ async def get_response(session, url, index):
 
     try:
         response = await session.get(url, headers=headers, timeout=120)
-    except requests.exceptions.SSLError:
+    except (requests.exceptions.SSLError, ssl.SSLError):
         # This error is raised when the website uses a legacy SSL version, which is not supported by requests
         if DEBUG:
             print("SSLError:", url)
@@ -147,6 +149,23 @@ async def get_response(session, url, index):
         if DEBUG:
            print(url, response)
 
+        content_type = None
+        try:
+            content_type = response.headers["content-type"]
+        except (KeyError, AttributeError):
+            pass
+
+        # If the response size is greater than 10 MB
+        # or the response is an unreadable content type
+        # or the response code from the website is not in the 200s
+        if response is not None and len(response.content) > 10000000 or content_type is not None and any(filtered_type in content_type for filtered_type in ["pdf", "excel", "msword", "image", "rtf", "zip", "octet", "csv", "json"]) or response is not None and not response.ok: 
+            # Discard the response content to prevent out of memory errors
+            if DEBUG:
+                print("Large or unreadable content discarded:", len(response.content), url)
+            new_response = requests.Response()
+            new_response.status_code = response.status_code
+            response = new_response
+        
         return {"index": index, "response": response}
 
 
@@ -159,34 +178,39 @@ async def render_js(urls_responses):
     print("Rendering JavaScript...")
     for url_response in tqdm(urls_responses):
         res = url_response["response"]
+        
+        if res is None or not res.ok:
+            continue
 
-        if res is not None and res.ok:
-            if DEBUG:
-                print("Rendering", url_response["url"][0])
+        if DEBUG:
+            print("Rendering", url_response["url"][0])
+        try:
             task = asyncio.create_task(res.html.arender())
-            
-            # Some websites will cause the rendering to hang indefinitely so we cancel the task if more than 15 seconds have elapsed
-            time_elapsed = 0
-            while not task.done():
-                time_elapsed += 1
-                await asyncio.sleep(0.1)
+        except AttributeError:
+            continue
+        
+        # Some websites will cause the rendering to hang indefinitely so we cancel the task if more than 15 seconds have elapsed
+        time_elapsed = 0
+        while not task.done():
+            time_elapsed += 1
+            await asyncio.sleep(0.1)
 
-                if time_elapsed > 150:
-                    task.cancel()
-                    break
-            
-            try:
-                await task
-            except (pyppeteer.errors.PageError, pyppeteer.errors.NetworkError) as e:
-                if DEBUG:
-                    print(f"{type(e).__name__}")
-            except Exception as e:
-                if DEBUG:
-                    print(traceback.format_exc())
-                    print(str(e))
-            except asyncio.CancelledError:
-                if DEBUG:
-                    print("Rendering cancelled")
+            if time_elapsed > 150:
+                task.cancel()
+                break
+        
+        try:
+            await task
+        except (pyppeteer.errors.PageError, pyppeteer.errors.NetworkError) as e:
+            if DEBUG:
+                print(f"{type(e).__name__}")
+        except Exception as e:
+            if DEBUG:
+                print(traceback.format_exc())
+                print(str(e))
+        except asyncio.CancelledError:
+            if DEBUG:
+                print("Rendering cancelled")
 
 
 def parse_response(url_response):
@@ -202,6 +226,8 @@ def parse_response(url_response):
     res = url_response["response"]
     tags["index"] = url_response["index"]
     tags["url"] = url_response["url"][0]
+    tags["html_title"] = ""
+    tags["meta_description"] = ""
 
     if res is None:
         tags["http_response"] = -1
@@ -212,8 +238,20 @@ def parse_response(url_response):
         return tags
 
     try:
-        soup = BeautifulSoup(res.html.html, "html.parser")
-    except (bs4.builder.ParserRejectedMarkup, AssertionError):
+        content_type = res.headers["content-type"]
+    except KeyError:
+        return tags
+
+    if "html" in content_type:
+        parser = "lxml"
+    elif "xml" in content_type:
+        parser = "lxml-xml"
+    else:
+        return tags
+
+    try:
+        soup = BeautifulSoup(res.html.html, parser)
+    except (bs4.builder.ParserRejectedMarkup, AssertionError, AttributeError):
         return tags
 
     tags["html_title"] = soup.title.string if soup.title is not None else ""
@@ -229,19 +267,31 @@ def parse_response(url_response):
         header_content = [header.text for header in headers]
         tags[header_tag] = json.dumps(header_content)
 
+    # Prevents most bs4 memory leaks
+    if soup.html:
+        soup.html.decompose()
+    
     return tags
 
 
 def collector_main(df, render_javascript=False):
-    header_tags_df = process_urls(df, render_javascript=render_javascript)
+    context = multiprocessing.get_context("spawn")
+    manager = context.Manager()
+    manager_list = manager.list([df])
+    
+    process = context.Process(target=process_urls, args=(manager_list, render_javascript))
+    process.start()
+    process.join()
 
-    return header_tags_df
+    return manager_list[0]
 
-def process_in_batches(df, batch_size=200):
+
+def process_in_batches(df, render_javascript=False, batch_size=200):
     """ Runs the tag collector on small batches of URLs contained in df
 
     Args: 
         df: polars dataframe containing all URLs in a 'url' column
+        render_javascript (bool): Whether or not to render webpage's JavaScript rendered HTML.
         batch_size: (int) # of URLs to process at a time (default 200)
 
     Returns:
@@ -268,7 +318,7 @@ def process_in_batches(df, batch_size=200):
         print(f"\nBatch {i + 1}/{num_batches}")
 
         # Call the collector_main function on the current batch
-        header_tags_df = collector_main(batch_df)
+        header_tags_df = collector_main(batch_df, render_javascript=render_javascript)
 
         # Check if it's the first batch, then directly set result_df
         # if not then append latest batch on bottom
@@ -282,18 +332,29 @@ def process_in_batches(df, batch_size=200):
 
 if __name__ == '__main__':
     # Open the input file and load the JSON data
-    with open(sys.argv[1]) as f:
-        data = json.load(f)
+    file = sys.argv[1]
 
-    df = pl.DataFrame(data)
+    if file.endswith(".json"):
+        with open(file) as f:
+            data = json.load(f)
+        df = pl.DataFrame(data)
+    elif file.endswith(".csv"):
+        df = pl.read_csv("train-urls.csv")#, skip_rows_after_header=3000)
+    else:
+        print("Unsupported filetype")
+        sys.exit(1)
     
+    render_javascript = False
+    if sys.argv[2] == "--render-javascript":
+        render_javascript = True
+
     #returns cumulative dataframe (contains all batches) of url and headers tags
-    cumulative_df = process_in_batches(df)
+    cumulative_df = process_in_batches(df, render_javascript=render_javascript)
 
     #join the url/header tag df with the original df which contains the labels (order has been preserved)
     #remove duplicate rows
     out_df = df.join(cumulative_df, on='url', how='left')
-    out_df = out_df.unique(subset=['id'], maintain_order=True)
+    out_df = out_df.unique(subset=['url'], maintain_order=True)
 
     if VERBOSE: print(out_df)
 
