f6d04268a42727096805f041b8278f61ee24077f
maxachis <maxachis@gmail.com>
2024-03-13 21:37:48 -0400

    Add comments to Label Studio Config

--- label_studio_interface/LabelStudioConfig.py
+++ label_studio_interface/LabelStudioConfig.py
@@ -3,8 +3,17 @@ from dotenv import load_dotenv
 
 
 class LabelStudioConfig:
+    """
+    This class is responsible for loading the configuration for the Label Studio API.
+    """
     def __init__(self, dotenv_file=".env"):
+        """
+
+        Args:
+            dotenv_file: the path to the .env file which contains the configuration for the Label Studio API
+        """
         load_dotenv(dotenv_file)
+        # Note that if the environment variables are not set, the default values, given below, are used
         self._project_id = os.getenv('LABEL_STUDIO_PROJECT_ID', '58475')
         self._organization_id = os.getenv('LABEL_STUDIO_ORGANIZATION_ID', '1')
         self._authorization_token = f'Token {os.getenv("LABEL_STUDIO_ACCESS_TOKEN", "abc123")}'
