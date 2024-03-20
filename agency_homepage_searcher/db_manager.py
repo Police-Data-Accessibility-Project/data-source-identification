a4003d9940e7bcd05449b4f50378a953262dcbad
maxachis <maxachis@gmail.com>
2024-03-20 08:37:57 -0400

    Create module and db manager

---
+++ agency_homepage_searcher/db_manager.py
@@ -0,0 +1,15 @@
+
+class DBManager:
+    """
+    Manages access to PostgreSQL database.
+    """
+
+    def __init__(self, db_name, db_user, db_password, db_host, db_port):
+        self.db_name = db_name
+        self.db_user = db_user
+        self.db_password = db_password
+        self.db_host = db_host
+        self.db_port = db_port
+        self.conn = None
+        self.cur = None
+        self.connect()
