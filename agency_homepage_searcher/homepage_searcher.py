dac99e0435679a9f8a9454d6aec96b229757130c
maxachis <maxachis@gmail.com>
2024-03-30 20:21:29 -0400

    Extract main function from homepage_searcher to main.py
    The main function of the application was moved from homepage_searcher.py to main.py to improve code organization. As part of the changes, the if __name__ == "__main__" clause was transferred to main.py. Also, environment variables were transferred to the GoogleSearcher, DBManager and HuggingFaceAPIManager constructors in main.py.

--- agency_homepage_searcher/homepage_searcher.py
+++ agency_homepage_searcher/homepage_searcher.py
@@ -265,28 +265,3 @@ class HomepageSearcher:
         self.update_search_cache(agency_ids)
 
 
-if __name__ == "__main__":
-    # Load the custom search API key and CSE ID from the .env file
-    load_dotenv()
-    google_searcher = GoogleSearcher(
-        api_key=os.getenv("CUSTOM_SEARCH_API_KEY"),
-        cse_id=os.getenv("CUSTOM_SEARCH_ENGINE_ID"))
-    db_manager = DBManager(
-        user=os.getenv("DIGITAL_OCEAN_DB_USERNAME"),
-        password=os.getenv("DIGITAL_OCEAN_DB_PASSWORD"),
-        host=os.getenv("DIGITAL_OCEAN_DB_HOST"),
-        port=os.getenv("DIGITAL_OCEAN_DB_PORT"),
-        db_name=os.getenv("DIGITAL_OCEAN_DB_NAME")
-    )
-    huggingface_api_manager = HuggingFaceAPIManager(
-        access_token=os.getenv("HUGGINGFACE_ACCESS_TOKEN"),
-        repo_id="PDAP/possible_homepage_urls"
-    )
-    homepage_searcher = HomepageSearcher(
-        search_engine=google_searcher,
-        database_manager=db_manager,
-        huggingface_api_manager=huggingface_api_manager
-    )
-    homepage_searcher.search_and_upload(
-        max_searches=100
-    )
