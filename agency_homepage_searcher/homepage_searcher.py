c372d66a97ee0e668d4a55e50185ff95050ec863
maxachis <maxachis@gmail.com>
2024-03-30 20:56:23 -0400

    Refactor homepage_searcher.py and improve error handling
    The code in homepage_searcher.py has been streamlined to improve readability and efficiency. The `search_until_quota_exceeded` method was renamed to `search_until_limit_reached` to more accurately describe its behavior, and the creation of `AgencyInfo` objects was moved to its own method for better abstraction. Thorough comments were added for each method to provide clear explanations. Additionally, error handling was enhanced to include the new QuotaExceededError.

--- agency_homepage_searcher/homepage_searcher.py
+++ agency_homepage_searcher/homepage_searcher.py
@@ -7,10 +7,12 @@ from typing import List, Union
 from dotenv import load_dotenv
 
 from agency_homepage_searcher.agency_info import AgencyInfo
-from agency_homepage_searcher.google_searcher import GoogleSearcher
+from agency_homepage_searcher.google_searcher import GoogleSearcher, QuotaExceededError
 from util.huggingface_api_manager import HuggingFaceAPIManager
 from util.db_manager import DBManager
 from util.miscellaneous_functions import get_filename_friendly_timestamp
+
+MAX_SEARCHES = 100  # Maximum searches to perform at a time when searching for results
 
 STATE_ISO_TO_NAME_DICT = {
     "AL": "Alabama",
@@ -119,45 +121,37 @@ class HomepageSearcher:
         self.database_manager = database_manager
         self.huggingface_api_manager = huggingface_api_manager
 
+    @staticmethod
+    def create_agency_info(agency_row: list) -> AgencyInfo:
+        """
+        Creates an AgencyInfo object using the provided agency data.
+        Args:
+            agency_row: Data row of the agency from the database.
+        Returns:
+            An AgencyInfo object.
+        """
+        try:
+            state_name = STATE_ISO_TO_NAME_DICT[agency_row[2]]
+        except KeyError:
+            raise ValueError(f"Invalid state ISO code: {agency_row[2]}")
+        return AgencyInfo(
+            agency_name=agency_row[0],
+            city=agency_row[3],
+            state=state_name,
+            county=agency_row[4],
+            zip_code=agency_row[7],
+            website=None,
+            agency_type=agency_row[1],
+            agency_id=agency_row[5]
+        )
+
     def get_agencies_without_homepage_urls(self) -> list[AgencyInfo]:
         """
         Retrieves a list of agencies without homepage URLs.
         Returns: list[AgencyInfo]
         """
         agency_rows = self.database_manager.execute(SQL_GET_AGENCIES_WITHOUT_HOMEPAGE_URLS)
-        results = []
-
-        for agency_row in agency_rows:
-            try:
-                state_name = STATE_ISO_TO_NAME_DICT[agency_row[2]]
-            except KeyError:
-                raise ValueError(f"Invalid state ISO code: {agency_row[2]}")
-            agency_info = AgencyInfo(
-                agency_name=agency_row[0],
-                city=agency_row[3],
-                state=state_name,
-                county=agency_row[4],
-                zip_code=agency_row[7],
-                website=None,
-                agency_type=agency_row[1],
-                agency_id=agency_row[5]
-            )
-            results.append(agency_info)
-        return results
-
-    @staticmethod
-    def build_search_string(agency_info: AgencyInfo) -> str:
-        """
-        Builds the search string which will be used in the search engine search
-        Args:
-            agency_info:
-
-        Returns:
-
-        """
-        search_string = (f"{agency_info.agency_name} {agency_info.city} {agency_info.state} {agency_info.county} "
-                         f"{agency_info.zip_code} {agency_info.website} {agency_info.agency_type}")
-        return search_string
+        return [self.create_agency_info(agency_row) for agency_row in agency_rows]
 
     def search(self, agency_info: AgencyInfo) -> Union[SearchResults, None]:
         """
@@ -166,24 +160,36 @@ class HomepageSearcher:
             agency_info: information about the agency
         Returns: either the search results or None if the quota is exceeded
         """
-        search_string = self.build_search_string(agency_info)
-        search_results = self.search_engine.search(search_string)
-        if search_results is None:  # Quota exceeded
+        try:
+            search_results = self.search_engine.search(
+                query=agency_info.get_search_string()
+            )
+            first_ten_results = self._get_first_ten_results(search_results)
+            return SearchResults(agency_id=agency_info.agency_id, search_results=first_ten_results)
+        except QuotaExceededError:
+            print("Quota exceeded")
             return None
-        # For now, return the first 10 results
-        search_result = SearchResults(
-            agency_id=agency_info.agency_id,
-            search_results=[PossibleHomepageURL(url=result['link'], snippet=result['snippet']) for result in
-                            search_results])
-        return search_result
 
-    def search_until_quota_exceeded(
+    @staticmethod
+    def _get_first_ten_results(results: list[dict]):
+        """
+        Extracts first ten results and forms a list of PossibleHomepageURL objects.
+
+        Args:
+        - results: A list that fetches from the search engine.
+
+        Returns:
+        - List[PossibleHomepageURL]: list containing first ten or less elements.
+        """
+        return [PossibleHomepageURL(url=result['link'], snippet=result['snippet']) for result in results[:10]]
+
+    def search_until_limit_reached(
             self,
             agency_info_list: list[AgencyInfo],
-            max_searches: int = 100
+            max_searches: int = MAX_SEARCHES
     ) -> list[SearchResults]:
         """
-        Searches for possible homepage URLs for agencies until the quota is exceeded.
+        Searches for possible homepage URLs for agencies until the limit is reached.
         Args:
             agency_info_list: list[AgencyInfo] - the list of agencies to search
             max_searches: int - the maximum number of searches to perform
@@ -196,12 +202,25 @@ class HomepageSearcher:
             try:
                 search_result = self.search(agency_info)
             except Exception as e:
-                print(f"An error occurred while searching for {agency_info}: {e}")
-                print("Returning existing search results")
-                return search_results
+                return self._handle_search_error(e, search_results)
             if search_result is None:  # Quota exceeded
                 break
             search_results.append(search_result)
+        return search_results
+
+    @staticmethod
+    def _handle_search_error(error: Exception, search_results: list[SearchResults]) -> list[SearchResults]:
+        """
+        Handles search error and returns existing search results.
+
+        Args:
+            error (Exception): The error that occurred while searching.
+            search_results (list[SearchResults]): The existing search results.
+        Returns:
+            list[SearchResults]: The existing search results.
+        """
+        print(f"An error occurred while searching: {error}")
+        print("Returning existing search results")
         return search_results
 
     def write_to_temporary_csv(self, data: List[SearchResults]) -> Path:
@@ -218,14 +237,31 @@ class HomepageSearcher:
             # Write the header
             writer.writerow(["agency_id", "url", "snippet"])
             for search_result in data:
-                try:
-                    for possible_homepage_url in search_result.search_results:
-                        writer.writerow([search_result.agency_id, possible_homepage_url.url, possible_homepage_url.snippet])
-                except Exception as e:
-                    raise(f"An unexpected error occurred while writing search results for {search_result.agency_id}: {e}")
+                self._write_search_result_to_csv(search_result, writer)
             # Remember the file name for later access
             temp_file_path = Path(tmpfile.name)
         return temp_file_path
+
+    @staticmethod
+    def _write_search_result_to_csv(search_result: SearchResults, writer: csv.writer) -> None:
+        """
+        Args:
+            search_result (SearchResults): An object that contains the search results.
+            writer (csv.writer): A writer object used to write the search results to a CSV file.
+
+        Raises:
+            Exception: If an unexpected error occurs while writing the search results.
+
+        Example:
+            search_result = SearchResults()
+            writer = csv.writer(...)
+            _write_search_result_to_csv(search_result, writer)
+        """
+        try:
+            for possible_homepage_url in search_result.search_results:
+                writer.writerow([search_result.agency_id, possible_homepage_url.url, possible_homepage_url.snippet])
+        except Exception as e:
+            raise f"An unexpected error occurred while writing search results for {search_result.agency_id}: {e}"
 
     def update_search_cache(self, agency_ids: list[str]) -> None:
         """
@@ -233,12 +269,23 @@ class HomepageSearcher:
         Args:
             agency_ids: list[str] - the agency IDs to update
         """
-        self.database_manager.executemany(
-            SQL_UPDATE_CACHE,
-            [(agency_id,) for agency_id in agency_ids]
-        )
+        parameters = [(agency_id,) for agency_id in agency_ids]
+        self.database_manager.executemany(SQL_UPDATE_CACHE, parameters)
 
-    def search_and_upload(self, max_searches: int = 100) -> None:
+    def _try_search_agency_info(self, agency_info: AgencyInfo) -> Union[SearchResults, List]:
+        """
+        Args:
+            agency_info: The agency information to be searched.
+
+        Returns:
+            The result of the search operation, or an empty list if an error occurs during the search.
+        """
+        try:
+            return self.search(agency_info)
+        except Exception as e:
+            return self._handle_search_error(e, [])
+
+    def search_and_upload(self, max_searches: int = MAX_SEARCHES) -> None:
         """
         Searches for possible homepage URLs for agencies without homepage URLs and uploads the results to HuggingFace.
         Args:
@@ -246,11 +293,8 @@ class HomepageSearcher:
         Returns: None
         """
         agencies = self.get_agencies_without_homepage_urls()
-        print("Searching for homepage URLs...")
-        search_results = self.search_until_quota_exceeded(
-            agency_info_list=agencies,
-            max_searches=max_searches
-        )
+        print(f"Searching for homepage URLs for first {max_searches} agencies...")
+        search_results = self.search_until_limit_reached(agency_info_list=agencies, max_searches=max_searches)
         print(f"Obtained {len(search_results)} search results")
         temp_file_path = self.write_to_temporary_csv(search_results)
         timestamp = get_filename_friendly_timestamp()
@@ -260,8 +304,5 @@ class HomepageSearcher:
         )
         print(f"Uploaded {len(search_results)} search results to HuggingFace: {temp_file_path}")
         temp_file_path.unlink()  # Clean up the temporary file
-        # Get the id of all agencies that were searched
         agency_ids = [search_result.agency_id for search_result in search_results]
         self.update_search_cache(agency_ids)
-
-
