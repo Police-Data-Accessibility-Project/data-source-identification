4af2e5a49eb1fe72779b7d1c59d69a905d2e66d1
maxachis <maxachis@gmail.com>
2024-04-04 07:39:32 -0400

    Update database setup script in tests
    Enhancements to the database setup script in the tests directory include stopping and removing previous running containers before creating and running a new one. This script now also builds the Docker image without using cache and sets up PostgreSQL's superuser username and password during the Docker run command.

--- Tests/test_database_setup/start_database_setup.sh
+++ Tests/test_database_setup/start_database_setup.sh
@@ -1,19 +1,28 @@
+# Stop the container if it is running
+docker stop test-postgres
+
+# Remove the container
+docker rm test-postgres
+
 # docker command to build a new image
 # Tag the image with the name "custom-test-postgres"
 # The build context is the current directory (indicated by ".")
-docker build \
+docker build --no-cache \
 -t custom-test-postgres \
 .
 
 # Docker command to run a container
 # --name: Assign a name to the container as "test-postgres"
 # --env-file: Specify a file that contains environment variables to be passed to the container
+# -e POSTGRES_USER=myuser and -e POSTGRES_PASSWORD=mypassword: set the PostgreSQL superuser's username and password, respectively.
 # -d: Run the container in detached mode which means it runs in the background and doesn't output logs to the terminal
 # -p: Publish the container’s port(s) 5432 to the host 5432
 # (last line): Specify the name of the docker image to run
 docker run \
 --name test-postgres \
 --env-file ./setup.env \
+-e POSTGRES_USER=myuser \
+-e POSTGRES_PASSWORD=mypassword \
 -d \
 -p 5432:5432 \
 custom-test-postgres
