bc59250cfec4b68e78ae47adbe02baefa3afdeb4
maxachis <maxachis@gmail.com>
2024-04-07 20:34:55 -0400

    Refactor test_dev_database.py for database host configuration
    Modified the code to configure the database host setting in test_dev_database.py by replacing static information with environment variables. This update ensures greater flexibility across different contexts, by loading host details from environment variables, and includes a new function to handle the creation of the Postgres factory.

--- Tests/test_dev_database.py
+++ Tests/test_dev_database.py
@@ -1,8 +1,7 @@
-from pathlib import Path
+import os
 
-import pytest
 from pytest_postgresql import factories
-from pytest_postgresql.janitor import DatabaseJanitor
+from dotenv import load_dotenv
 
 from tests.database.ProdSchemaManager import ProdSchemaManager
 
@@ -10,15 +9,27 @@ from tests.database.ProdSchemaManager import ProdSchemaManager
 This requires a postgresql docker container set up and listening on port 5432"
 The setup should mirror what is in start_database_setup.sh
 """
-#
-prod_schema_manager = ProdSchemaManager()
-postgresql_in_docker = factories.postgresql_noproc(
-    port="5432",
-    user="myuser",
-    password="mypassword",
-    load=[prod_schema_manager.schema_path]
-)
+
+
+def create_postgresql_factory():
+    load_dotenv()
+
+    host = os.getenv("TEST_DATABASE_HOST")
+    if host is None:
+        raise ValueError("TEST_DATABASE_HOST environment variable is not set.")
+    prod_schema_manager = ProdSchemaManager()
+    return factories.postgresql_noproc(
+        port="5432",
+        user="myuser",
+        password="mypassword",
+        host=host,
+        load=[prod_schema_manager.schema_path]
+    )
+
+
+postgresql_in_docker = create_postgresql_factory()
 postgresql = factories.postgresql("postgresql_in_docker")
+
 
 def test_get_agencies_without_homepage_urls(postgresql):
     cur = postgresql.cursor()
