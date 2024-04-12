df0c53e64a933c8a94230afda122dccf3505e36a
maxachis <maxachis@gmail.com>
2024-04-12 17:07:41 -0400

    Create empty test_google_searcher_integration.py

---
+++ Tests/test_google_searcher_integration.py
@@ -0,0 +1,8 @@
+
+
+
+
+"""
+Test that executed_datetime is updated in search_queue when a new result is added to search_results
+Test both that the first insert updates it from null, and the second insert has a datetime that is after the first.
+"""
