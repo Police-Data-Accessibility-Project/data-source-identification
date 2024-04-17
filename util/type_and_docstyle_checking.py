b34a2ed9590022f1de79b81d760cf5a54c751a3e
maxachis <maxachis@gmail.com>
2024-04-17 16:50:58 -0400

    Refactor type and docstyle checking script
    Enhanced the script to provide a clearer error reporting method. It now segregates 'mypy' and 'pydocstyle' errors into distinct sections. Furthermore, the script can find and use a ".pydocstyle" configuration file if present at the repository root.

--- util/type_and_docstyle_checking.py
+++ util/type_and_docstyle_checking.py
@@ -1,15 +1,32 @@
+"""
+This module contains the functionality for
+running type and docstyle checking on the repository
+"""
+
+import re
 import subprocess
 import os
 
-import pytest
+from util.miscellaneous_functions import get_project_root
 
 MYPY_COMMAND = 'mypy --ignore-missing-imports --disallow-untyped-defs '
-PYDOCSTYLE_COMMAND = 'pydocstyle '
 
-ERROR_KEYWORD = "error"
+MYPY_ERROR_KEYWORD = "error"
+PYDOCSTYLE_ERROR_KEYWORD = "missing"
 
-def find_pydocstyle_config():
 
+def find_pydocstyle_config() -> str:
+    """
+    Find the .pydocstyle config file, located at the root of the repository
+    and return the --config argument along with the path
+    """
+    root_path = get_project_root()
+    config_file = os.path.join(root_path, ".pydocstyle")
+    if os.path.exists(config_file):
+        return f"--config={config_file} "
+
+
+PYDOCSTYLE_COMMAND = 'pydocstyle ' + find_pydocstyle_config()
 
 
 def run_command(command: str) -> str:
@@ -41,11 +58,8 @@ def find_modified_python_files() -> list[str]:
     return modified_files
 
 
-def get_file_string(modified_files: list[str]) -> str:
-    return '\'' + "\' \'".join(modified_files) + '\''
-
-
-def run_and_get_errored_outputs(files_to_check: list[str], command: str):
+def run_and_get_errored_outputs(files_to_check: list[str], command: str) -> list[str]:
+    """Run a given command on the specified files and return the outputs, if they include errors"""
     errored_outputs = []
     for file_to_check in files_to_check:
         output = run_command(command + ' ' + file_to_check)
@@ -53,30 +67,41 @@ def run_and_get_errored_outputs(files_to_check: list[str], command: str):
             errored_outputs.append(output)
     return errored_outputs
 
-def has_errors(output):
-    return ERROR_KEYWORD in output.lower()
+
+def has_errors(output: str) -> bool:
+    """Checks for specific keywords to indicate the presence of errors"""
+    return MYPY_ERROR_KEYWORD in output.lower() or PYDOCSTYLE_ERROR_KEYWORD in output.lower()
 
 
-def check_files(files_to_check: list[str]):
+def check_files(files_to_check: list[str]) -> tuple[list[str], list[str]]:
+    """
+    Checks the specified files to see if they contain mypy or pydocstyle errors
+    Returns those errors if they exist
+    """
     if files_to_check:
-
-        mypy_outputs = run_and_get_errored_outputs(files_to_check, MYPY_COMMAND)
-        print("Mypy Results:\n")
-        for output in mypy_outputs:
-            output = output.replace('\nFound 1 error in 1 file (checked 1 source file)', '')
-            print(output)
-        pydocstyle_outputs = run_and_get_errored_outputs(files_to_check, PYDOCSTYLE_COMMAND)
-        print("Pydocstyle Results:\n")
-        for output in pydocstyle_outputs:
-            print(output)
-
-        # if has_errors(mypy_result) or has_errors(pydocstyle_result):
-        #     print("Errors detected in mypy or pydocstyle")
-        #     exit(1)
+        mypy_errors = run_and_get_errored_outputs(files_to_check, MYPY_COMMAND)
+        pydocstyle_errors = run_and_get_errored_outputs(files_to_check, PYDOCSTYLE_COMMAND)
+        return mypy_errors, pydocstyle_errors
     else:
         print("No Python files were modified.")
+        return [], []
+
+
+def announce_errors(mypy_errors: list[str], pydocstyle_errors: list[str]):
+    """Announce errors if present"""
+    print("\nMYPY RESULTS:\n")
+    for output in mypy_errors:
+        output = re.sub(r'\nFound \d+ errors* in 1 file \(checked 1 source file\)', '', output)
+        print(output)
+    print("\nPYDOCSTYLE RESULTS:\n")
+    for output in pydocstyle_errors:
+        print(output)
 
 
 if __name__ == "__main__":
     files = find_modified_python_files()
-    check_files(files)
+    mypy_errors, pydocstyle_errors = check_files(files)
+    announce_errors(mypy_errors, pydocstyle_errors)
+    if len(mypy_errors) > 0 or len(pydocstyle_errors) > 0:
+        print("Errors detected in mypy or pydocstyle")
+        exit(1)
