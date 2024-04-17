d5b91e6b4b5e69f149b90b95d85f33ebabe76d6d
maxachis <maxachis@gmail.com>
2024-04-17 16:46:39 -0400

    Add function to find project root directory
    Added a new function named 'get_project_root' in miscellaneous_functions.py. This function locates the root directory of the project by looking for root markers. It starts by checking the current file's directory and moving up levels until a root marker is found. This enhances code reusability and makes reference to root directory easier.

--- util/miscellaneous_functions.py
+++ util/miscellaneous_functions.py
@@ -25,6 +25,25 @@ def create_directories_if_not_exist(file_path: str):
         print(f"Creating directory: {directory}")
         os.makedirs(directory)
 
+def get_project_root() -> Path:
+    """
+    Returns the path to the project root as a Path object by searching for a marker.
+    Returns:
+
+    """
+    # Define the root markers that signify the root directory of the project
+    root_markers = ['.git']  # Add more markers as needed
+    # Start from the current file's directory
+    current_dir = Path(__file__).resolve().parent
+    while current_dir != current_dir.parent:  # Check if we've reached the root of the filesystem
+        if any((current_dir / marker).exists() for marker in root_markers):
+            # If a root marker is found, return this directory as a Path object
+            return current_dir
+        # Move up one directory level
+        current_dir = current_dir.parent
+    raise FileNotFoundError("Project root not found.")
+
+
 
 def get_file_path(file_name: str, directory: str = None) -> Path:
     """
