a21df3ff8effd259bf9e3f7231fa0ecb2ce79d79
maxachis <maxachis@gmail.com>
2024-04-20 15:38:37 -0400

    Add new dataclasses to Florida Data Request module
    New data classes have been introduced to the Florida Data Request module. The added classes, `SearchResult`, `QueryAndSearchResults`, and `MostRelevantResult`, are for organizing and managing search results generated from the queries.

--- data_request/florida_data_request/request_dataclasses.py
+++ data_request/florida_data_request/request_dataclasses.py
@@ -8,3 +8,23 @@ class QueryInfo:
     agency_id: str
     query_text: str
     query_type: QueryType
+
+@dataclass
+class SearchResult:
+    result_id: int
+    title: str
+    url: str
+    snippet: str
+
+@dataclass
+class QueryAndSearchResults:
+    query_id: int
+    query_text: str
+    search_results: list[SearchResult]
+
+
+@dataclass
+class MostRelevantResult:
+    result_id: int
+    rationale: str
+
