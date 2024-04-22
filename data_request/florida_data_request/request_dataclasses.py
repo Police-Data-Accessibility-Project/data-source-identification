216c9bae58e4bd582fc796248e9b7ffeb2d81fe1
maxachis <maxachis@gmail.com>
2024-04-22 07:44:25 -0400

    Revised QueryInfo class in request_dataclasses.py
    Replaced 'agency_id' with 'query_id' and removed 'query_type' in QueryInfo class in 'request_dataclasses.py'. Now, the `QueryInfo` class contains 'query_id' and 'query_text' attributes, which better aligns with the data structure that the application is expected to handle.

--- data_request/florida_data_request/request_dataclasses.py
+++ data_request/florida_data_request/request_dataclasses.py
@@ -5,9 +5,8 @@ from data_request.florida_data_request.request_enums import QueryType
 
 @dataclass
 class QueryInfo:
-    agency_id: str
+    query_id: int
     query_text: str
-    query_type: QueryType
 
 @dataclass
 class SearchResult:
