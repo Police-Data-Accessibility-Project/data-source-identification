41f5caabedb0c5a91b8bf8983dffea5cf9c975cd
Kylie <github.a5cee@kyscott.aleeas.com>
2024-05-02 12:02:00 -0600

    Formatting updates

--- hugging_face/url_relevance/clean_data.py
+++ hugging_face/url_relevance/clean_data.py
@@ -9,30 +9,45 @@ FILE = "train-urls-keywords.csv"
 
 with open(FILE, newline="") as readFile, open("new.csv", "w", newline="") as writeFile:
     reader = csv.DictReader(readFile)
-    fieldnames = ["url", "url_path", "label", "html_title", "meta_description", "root_page_title", "http_response", "keywords", "h1", "h2", "h3", "h4", "h5", "h6", "div_text"]
+    fieldnames = [
+        "url",
+        "url_path",
+        "label",
+        "html_title",
+        "meta_description",
+        "root_page_title",
+        "http_response",
+        "keywords",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "h6",
+        "div_text",
+    ]
     writer = csv.DictWriter(writeFile, fieldnames=fieldnames)
     writer.writeheader()
-    
+
     for row in reader:
         write_row = row
 
         for key, value in write_row.items():
             try:
-                l = ast.literal_eval(value)
+                val_list = ast.literal_eval(value)
             except (SyntaxError, ValueError):
                 continue
-            
-            #if key == "keywords":
+
+            # if key == "keywords":
             #    l = l[0:2]
-            
+
             try:
-                value = " ".join(l)
+                value = " ".join(val_list)
             except TypeError:
                 continue
 
-            write_row.update({key:value})
+            write_row.update({key: value})
 
         writer.writerow(write_row)
-    
+
     os.rename("new.csv", FILE)
-    
