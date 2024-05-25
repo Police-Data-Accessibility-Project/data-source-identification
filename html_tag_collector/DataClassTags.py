625147546254cc4b6d6a604da496903ce628ac44
Kylie <github.a5cee@kyscott.aleeas.com>
2024-05-24 23:48:17 -0600

    Add dataclass parameters

--- html_tag_collector/DataClassTags.py
+++ html_tag_collector/DataClassTags.py
@@ -3,5 +3,17 @@ from dataclasses import dataclass
 
 @dataclass
 class Tags:
-    html_title: str
-    
+    index: int = None
+    url: str = ""
+    url_path: str = ""
+    html_title: str = ""
+    meta_description: str = ""
+    root_page_title: str = ""
+    http_response: int = -1
+    h1: str = ""
+    h2: str = ""
+    h3: str = ""
+    h4: str = ""
+    h5: str = ""
+    h6: str = ""
+    div_text: str = ""
