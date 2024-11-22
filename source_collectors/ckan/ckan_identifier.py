241c6ac9db0b17e38174cbeede1ab4731265f7ab
Kylie <github.a5cee@kyscott.aleeas.com>
2024-11-21 18:24:01 -0700

    Create identifier

--- source_collectors/ckan/ckan_identifier.py
+++ source_collectors/ckan/ckan_identifier.py
@@ -1,27 +1,73 @@
 """This program identifies if a given URL is a CKAN-hosted website"""
-import re
 
+import re
+import sys
+
+import asyncio
 from bs4 import BeautifulSoup
 from from_root import from_root
+import polars as pl
 import requests
+from requests import Response
+
+p = from_root(".gitignore").parent
+sys.path.insert(1, str(p))
+
+from html_tag_collector.collector import run_get_response
 
 
-def is_ckan_hosted(url):
-    head = requests.get(url)
-    soup = BeautifulSoup(head.content, "lxml")
-    
+def get_responses(urls: list[str]) -> list[Response]:
+    """Uses the tag collector's run_get_response method to get response objects for each url.
+
+    :param urls: The list of urls.
+    :return: The list of resulting responses.
+    """
+    loop = asyncio.get_event_loop()
+    future = asyncio.ensure_future(run_get_response(urls))
+    loop.run_until_complete(future)
+    return future.result()
+
+
+def is_ckan_hosted(response: Response) -> bool:
+    """Checks if the response content contains the CKAN version tag.
+
+    :param response: The response object.
+    :return: True if the CKAN version tag is found, False otherwise.
+    """
+    soup = BeautifulSoup(response.content, "lxml")
+
+    # Checks if the CKAN version tag is present, looks like this:
+    # <meta name="generator" content="ckan 2.10.5">
     ckan_tag = soup.head.find(content=re.compile("ckan \d+\.\d+\.\d+"))
     if ckan_tag is not None:
         return True
-    
+
     return False
 
 
 def main():
-    url = "https://www.w3schools.com/python/python_regex.asp"
-    print(is_ckan_hosted(url))
+    file = sys.argv[1]
+    df = pl.read_csv(file)
+
+    results = get_responses(urls=list(df["url"]))
+
+    results_df = pl.from_dicts(results)
+    urls_and_responses = pl.DataFrame(
+        [
+            pl.Series("url", df["url"]),
+            pl.Series("response", results_df["response"]),
+        ]
+    )
+
+    # Add a new column indicating if the URL contains the CKAN version tag
+    urls_and_responses = urls_and_responses.with_columns(
+        pl.col("response")
+        .map_elements(is_ckan_hosted, return_dtype=bool)
+        .alias("is_ckan_hosted")
+    )
+
+    urls_and_responses.select(["url", "is_ckan_hosted"]).write_csv("output.csv")
 
 
 if __name__ == "__main__":
     main()
-    
