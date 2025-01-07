56bf6035092d18c8ae2c4f2ddc58c20449a23d05
Max Chis <maxachis@gmail.com>
2025-01-07 17:49:33 -0500

    Update `target_metadata` to reference `Base` in `models.py`

--- collector_db/alembic/env.py
+++ collector_db/alembic/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from collector_db.helper_functions import get_postgres_connection_string
+from collector_db.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -20,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
