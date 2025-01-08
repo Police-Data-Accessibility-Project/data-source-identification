c2be66e4ef5bc1d0fd9f455408a3b6292ef6d59a
Max Chis <maxachis@gmail.com>
2025-01-08 14:30:40 -0500

    Update db_client_test to include alembic upgrades/downgrades in setup/teardown

--- tests/test_automated/integration/conftest.py
+++ tests/test_automated/integration/conftest.py
@@ -1,5 +1,8 @@
 
 import pytest
+from alembic import command
+from alembic.config import Config
+from sqlalchemy import create_engine
 
 from collector_db.helper_functions import get_postgres_connection_string
 from collector_db.models import Base
@@ -11,10 +14,21 @@ from core.SourceCollectorCore import SourceCollectorCore
 
 @pytest.fixture
 def db_client_test() -> DatabaseClient:
-    db_client = DatabaseClient(db_url=get_postgres_connection_string())
+    conn = get_postgres_connection_string()
+    alembic_cfg = Config("alembic.ini")
+    engine = create_engine(conn)
+    alembic_cfg.attributes["connection"] = engine.connect()
+
+    command.upgrade(alembic_cfg, "head")
+
+    Base.metadata.create_all(engine)
+
+    db_client = DatabaseClient(db_url=conn)
     yield db_client
     db_client.engine.dispose()
     Base.metadata.drop_all(db_client.engine)
+    command.stamp(alembic_cfg, "base")
+
 
 @pytest.fixture
 def test_core(db_client_test):
