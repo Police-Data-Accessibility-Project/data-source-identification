3e7bff14f4f8fa521771d3c88d8c3de51a01e9a1
Max Chis <maxachis@gmail.com>
2025-01-18 09:04:48 -0500

    Clean up HF Interface, update associated tests

--- hugging_face/HuggingFaceInterface.py
+++ hugging_face/HuggingFaceInterface.py
@@ -9,27 +9,22 @@ class HuggingFaceInterface:
     def __init__(self):
         self.pipe = pipeline("text-classification", model="PDAP/url-relevance")
 
-    # TODO: To be removed once `get_url_relevancy` fully implemented
-    def get_url_relevancy_old(self, urls: list[str], threshold: float = 0.8) -> list[bool]:
-        results: list[list[dict]] = self.pipe(urls)
-        for result, url in zip(results, urls):
-            result.append({"url": url})
+    def get_url_relevancy(
+            self,
+            urls_with_html: list[URLWithHTML],
+            threshold: float = 0.5
+    ) -> list[bool]:
+        urls = [url_with_html.url for url_with_html in urls_with_html]
+        results: list[dict] = self.pipe(urls)
 
         bool_results = []
         for result in results:
-            score = result[0]["score"]
+            score = result["score"]
             if score >= threshold:
                 bool_results.append(True)
             else:
                 bool_results.append(False)
         return bool_results
-
-    def get_url_relevancy(
-            self,
-            urls_with_html: list[URLWithHTML],
-            threshold: float = 0.8
-    ) -> list[bool]:
-        raise NotImplementedError
 
 
 
--- tests/manual/huggingface/test_hugging_face_interface.py
+++ tests/manual/huggingface/test_hugging_face_interface.py
@@ -1,10 +1,17 @@
+from collector_db.DTOs.URLWithHTML import URLWithHTML
 from hugging_face.HuggingFaceInterface import HuggingFaceInterface
 
 
 def test_get_url_relevancy():
     hfi = HuggingFaceInterface()
-    result = hfi.get_url_relevancy("https://coloradosprings.gov/police-department/article/news/i-25-traffic-safety-deployment-after-stop")
-    result = hfi.get_url_relevancy("https://example.com")
+    def package_url(url: str) -> URLWithHTML:
+        return URLWithHTML(url=url, url_id=1, html_infos=[])
 
-    result = hfi.get_url_relevancy("https://police.com")
+    result = hfi.get_url_relevancy([
+        package_url("https://coloradosprings.gov/police-department/article/news/i-25-traffic-safety-deployment-after-stop"),
+        package_url("https://example.com"),
+        package_url("https://police.com")
+    ])
+    print(result)
+
 
