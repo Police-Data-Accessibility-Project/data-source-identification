6a30a43348f8d8215d3f5ebeb6df2585a6762984
Max Chis <maxachis@gmail.com>
2025-01-28 18:26:35 -0500

    Remove unused files

--- core/DTOs/LabelStudioExportResponseInfo.py
+++
@@ -1,9 +0,0 @@
-from typing import Annotated
-
-from fastapi.param_functions import Doc
-from pydantic import BaseModel
-
-
-class LabelStudioExportResponseInfo(BaseModel):
-    label_studio_import_id: Annotated[int, Doc("The ID of the Label Studio import")]
-    num_urls_imported: Annotated[int, Doc("The number of URLs imported")]
