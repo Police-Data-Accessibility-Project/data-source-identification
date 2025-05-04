02f7b3a43a2db4f76ee620c08fc92d2c30ce5cc6
Max Chis <maxachis@gmail.com>
2025-05-03 21:23:30 -0400

    Comment out URL relevance Huggingface Task Operator call

--- core/TaskManager.py
+++ core/TaskManager.py
@@ -99,7 +99,7 @@ class TaskManager:
     async def get_task_operators(self) -> list[TaskOperatorBase]:
         return [
             await self.get_url_html_task_operator(),
-            await self.get_url_relevance_huggingface_task_operator(),
+            # await self.get_url_relevance_huggingface_task_operator(),
             await self.get_url_record_type_task_operator(),
             await self.get_agency_identification_task_operator(),
             await self.get_url_miscellaneous_metadata_task_operator(),
