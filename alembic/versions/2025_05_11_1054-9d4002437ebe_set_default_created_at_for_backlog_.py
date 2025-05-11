7de9c50a90532bf3db3c3a44bca32db29a3f451c
Max Chis <maxachis@gmail.com>
2025-05-11 10:54:53 -0400

    DRAFT

---
+++ alembic/versions/2025_05_11_1054-9d4002437ebe_set_default_created_at_for_backlog_.py
@@ -0,0 +1,38 @@
+"""Set default created_at for backlog_snapshot
+
+Revision ID: 9d4002437ebe
+Revises: 6f2007bbcce3
+Create Date: 2025-05-11 10:54:22.797147
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '9d4002437ebe'
+down_revision: Union[str, None] = '6f2007bbcce3'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.alter_column(
+        table_name='backlog_snapshots',
+        column_name='created_at',
+        existing_type=sa.DateTime(),
+        nullable=False,
+        server_default=sa.text('now()')
+    )
+
+
+def downgrade() -> None:
+    op.alter_column(
+        table_name='backlog_snapshots',
+        column_name='created_at',
+        existing_type=sa.DateTime(),
+        nullable=False,
+        server_default=None
+    )
