42e4b34e9b57cef3a78d167771e3798a202cbf95
maxachis <maxachis@gmail.com>
2025-07-26 11:38:37 -0400

    Convert from `Optional` to `| None` type hint

--- src/core/tasks/url/operators/submit_approved_url/tdo.py
+++ src/core/tasks/url/operators/submit_approved_url/tdo.py
@@ -1,9 +1,9 @@
-from typing import Optional
+from datetime import datetime
 
 from pydantic import BaseModel
 
 from src.core.enums import RecordType
-from datetime import datetime
+
 
 class SubmitApprovedURLTDO(BaseModel):
     url_id: int
@@ -11,16 +11,16 @@ class SubmitApprovedURLTDO(BaseModel):
     record_type: RecordType
     agency_ids: list[int]
     name: str
-    description: str
+    description: str | None = None
     approving_user_id: int
-    record_formats: Optional[list[str]] = None
-    data_portal_type: Optional[str] = None
-    supplying_entity: Optional[str] = None
-    data_source_id: Optional[int] = None
-    request_error: Optional[str] = None
+    record_formats: list[str] | None = None
+    data_portal_type: str | None = None
+    supplying_entity: str | None = None
+    data_source_id: int | None = None
+    request_error: str | None = None
 
 class SubmittedURLInfo(BaseModel):
     url_id: int
-    data_source_id: Optional[int]
-    request_error: Optional[str]
-    submitted_at: Optional[datetime] = None
+    data_source_id: int | None
+    request_error: str | None
+    submitted_at: datetime | None = None
