8ccd1b6fc58c4c74f6c14165a3d0633d1c76eefa
Max Chis <maxachis@gmail.com>
2025-08-11 09:21:50 -0400

    Change name of url_data_sources to url_data_source

--- src/db/models/instantiations/url/data_source/sqlalchemy.py
+++ src/db/models/instantiations/url/data_source/sqlalchemy.py
@@ -6,7 +6,7 @@ from src.db.models.templates_.with_id import WithIDBase
 
 
 class URLDataSource(CreatedAtMixin, URLDependentMixin, WithIDBase):
-    __tablename__ = "url_data_sources"
+    __tablename__ = "url_data_source"
 
     data_source_id = Column(Integer, nullable=False)
 
